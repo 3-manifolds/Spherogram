a169b6847e32f0f642ce21f26f3988deb00cf848
Marc Culler <culler@math.uic.edu>
2013-05-10 18:37:18 -0500

    Added spherogram.__all__ .

--- spherogram_src/__init__.py
+++ spherogram_src/__init__.py
@@ -2,3 +2,14 @@ from .graphs import *
 from .presentations import *
 from .links import * 
 from .codecs import *
+try:
+    import snappy
+except ImportError:
+    pass
+
+__all__ = ['ABC', 'CyclicList', 'CyclicWord', 'DTcodec', 'Digraph',
+           'DirectedEdge', 'DirectedMultiEdge', 'Edge', 'FatEdge',
+           'FatGraph', 'Graph', 'IdentityBraid', 'InfinityTangle',
+           'Link', 'MultiEdge', 'Poset', 'Presentation',
+           'RationalTangle', 'Strand', 'Tangle','WhiteheadMove',
+           'Word', 'ZeroTangle']
