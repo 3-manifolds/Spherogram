1f6cb1a631b6f4b10cda8e171d0bd9f451b0b99e
Marc Culler <culler@math.uic.edu>
2013-06-07 18:41:38 -0500

    Removed -arch ppc when building planarity.

--- planarity_src/build_planarity.sh
+++ planarity_src/build_planarity.sh
@@ -4,8 +4,8 @@ cd planarity-read-only
 patch -p0 < ../planarity.patch
 cd c
 if [ `uname` = "Darwin" ]; then
-  gcc -arch i386 -arch ppc -arch x86_64 -c *.c nauty/*.c
-  gcc -arch i386 -arch ppc -arch x86_64 -o planarity *.o
+  gcc -arch i386 -arch x86_64 -c *.c nauty/*.c
+  gcc -arch i386 -arch x86_64 -o planarity *.o
 else
   gcc -c -fPIC *.c nauty/*.c
   gcc -o planarity *.o
