2f6da266f3e9ccb976489efa71aef5052b3d7f20
Nathan Dunfield <nathan@dunfield.info>
2013-06-23 20:49:50 -0500

    First semi-working version of link drawing!

--- dev/orthogonal/orthogonal.py
+++ dev/orthogonal/orthogonal.py
@@ -1,111 +1,27 @@
 """
 Orthogonal layouts for link diagrams
 
-Finds a layout of the given link diagram where the strands are unions of edges in the standard integer grid, following:
+Finds a layout of the given link diagram where the strands are unions of
+edges in the standard integer grid, following:
 
-Tamassia, On embedding a graph in the grid with the minimum number of bends.
-Siam J. Comput. 16 (1987)
-http://dx.doi.org/10.1137/0216030
+Tamassia, On embedding a graph in the grid with the minimum number of
+bends. Siam J. Comput. 16 (1987) http://dx.doi.org/10.1137/0216030
 
 and
 
 Bridgeman et. al., Turn-Regularity and Planar Orthogonal Drawings
 ftp://ftp.cs.brown.edu/pub/techreports/99/cs99-04.pdf
 
-As all vertices (=crossings) of the underlying graph are 4-valent, things simpify;
-the associated network N(P) has A_V empty and A_F has no self-loops.  
+As all vertices (=crossings) of the underlying graph are 4-valent,
+things simpify; the associated network N(P) has A_V empty and A_F has no
+self-loops.
 """
 
-import spherogram, snappy, collections, networkx, random
-from spherogram.links.links import CrossingEntryPoint
+import spherogram, snappy, collections, networkx, random, plink
+from spherogram.links.links import CrossingStrand, CrossingEntryPoint, Strand
 
 
-    
-class Face:
-    def __init__(self, edges, exterior=False, bends=None, angles=None):
-        self.edges, self.exterior = edges, exterior
-        if bends is None:
-            bends = {e:'' for e in edges }
-        if angles is None:
-            angles = {e:None for e in edges}
-        self.bends, self.angles = bends, angles
-
-    def intersection(self, other):
-        return set(self.edges).intersection(set(other.edges))
-
-    def source_capacity(self):
-        if self.exterior:
-            return 0
-        return max(4 - len(self), 0)
-
-    def sink_capacity(self):
-        if self.exterior:
-            return len(self) + 4
-        return max(len(self) - 4, 0)
-
-    def __len__(self):
-        return len(self.edges)
-
-def all_faces(link):
-    ans = [Face(F) for F in link.faces()]
-    F = max(ans, key=len)
-    F.exterior = True
-    return ans
-
-def flow_network(link):
-    """
-    Tamassia's associated graph N(P) where the flow
-    problem resides.  
-    """
-    faces = all_faces(link)
-    vertices = {'s', 't'}.union(set(faces))
-    G = spherogram.Digraph(singles=vertices)
-    for A in faces:
-        for B in faces:
-            if A != B and A.intersection(B):
-                G.add_edge(A, B)
-
-    for F in faces:
-        c = F.source_capacity()
-        if c:
-            G.add_edge('s', F)
-
-        C = F.sink_capacity()
-        if C:
-            G.add_edge(F, 't')
-    
-    return G
-
-def flow_networkx(link):
-    """
-    Tamassia's associated graph N(P) where the flow
-    problem resides.  
-    """
-    faces = all_faces(link)
-    G = networkx.DiGraph()
-    
-    # Source
-    source_demand = sum(F.source_capacity() for F in faces)
-    G.add_node('s', demand = -source_demand)
-    for F in faces:
-        if F.source_capacity():
-            G.add_edge('s', faces.index(F), weight=0, capacity=F.source_capacity())
-
-    # Sink
-    sink_demand = sum(F.sink_capacity() for F in faces)
-    assert sink_demand == source_demand
-    G.add_node('t', demand = sink_demand)
-    for F in faces:
-        if F.sink_capacity():
-            G.add_edge( faces.index(F), 't', weight=0, capacity=F.sink_capacity())
-
-    # Rest of edges
-    for F0 in faces:
-        for F1 in faces:
-            if F0 != F1:
-                G.add_edge(faces.index(F0), faces.index(F1), weight=1)  # infinite capacity
-
-    return G
+#----- Utility code ------
 
 def element_map(partition):
     ans = dict()
@@ -114,17 +30,27 @@ def element_map(partition):
             ans[x] = P
     return ans
 
+def appearances_dict(list_of_lists):
+    ans = dict()
+    for L in list_of_lists:
+        for x in L:
+            if not ans.has_key(x):
+                ans[x] = [L]
+            else:
+                ans[x].append(L)
+
+    return ans
+
 def partial_sums(L):
     ans, sum = [], 0
     for x in L:
         sum += x
         ans.append(sum)
     return ans
-        
+
 def topological_numbering(G):
     """
-    Finds an optimal weighted topological
-    numbering a directed acyclic graph
+    Finds an optimal weighted topological numbering a directed acyclic graph
     """
     in_valences = { v:G.in_valence(v) for v in G.vertices }
     numbering = {}
@@ -145,12 +71,235 @@ def topological_numbering(G):
 
     return numbering
 
+#----- Faces of link diagrams ------
+
+class DummyVertex:
+    def __repr__(self):
+        return "<DummyVertex>"
+
+def _subdivide_edge(self, crossing_strand, n):
+    """
+    Given a CrossingStrand, subdivides the edge for which it's the *head*
+    into (n + 1) pieces.
+    
+    WARNING: this breaks several of the link's internal data structures.
+    """
+    head = crossing_strand
+    backwards = not (head in head.crossing.entry_points())
+    if backwards:
+        head = head.opposite()
+    tail = head.opposite()
+    strands = [Strand() for i in range(n)]
+    strands[0][0] = tail.crossing[tail.entry_point]
+    for i in range(n - 1):
+        strands[i][1] = strands[i+1][0]
+    strands[-1][1] = head.crossing[head.entry_point]
+        
+spherogram.Link._subdivide_edge = _subdivide_edge
+    
+class Face(list):
+    def __init__(self, link, crossing_strands, exterior=False):
+        list.__init__(self, crossing_strands)
+        self.edges = {link.CS_to_edge[c]:c for c in crossing_strands}
+        self.exterior = exterior
+        self.angles =[90 for e in self] 
+
+    def edge_of_intersection(self, other):
+        """
+        Returns edge as the pair of CrossingStrands for self and other which
+        defines the common edge. Returns None if the faces are disjoint.
+        """
+        common_edges = set(self.edges) & set(other.edges)
+        if common_edges:
+            e = common_edges.pop()
+            return (self.edges[e], other.edges[e])
+            
+    def source_capacity(self):
+        if self.exterior:
+            return 0
+        return max(4 - len(self), 0)
+
+    def sink_capacity(self):
+        if self.exterior:
+            return len(self) + 4
+        return max(len(self) - 4, 0)
+
+    def iterate_from(self, edge):
+        i = self.index(edge)
+        return zip(self[i:] + self[:i], self.angles[i:] + self.angles[:i])
+
+    def bend(self, edge, angles):
+        """
+        Assumes edge has already been subdivided by adding in len(angles) new
+        vertices. 
+        """
+        i = self.index(edge)
+        for a in reversed(angles):
+            edge = edge.previous_corner()
+            self.insert(i, edge)
+            self.angles.insert(i, a)
+
+    def orient_edges(self, edge, orientation):
+        """
+        Returns a dictionary of the induced directions of the edges (and their
+        opposites) around the face.
+        """
+        dirs = ["left", "up", "right", "down"]
+        dir = dirs.index(orientation)
+        ans = dict()
+        for e, a in self.iterate_from(edge):
+            ans[e] = dirs[dir]
+            ans[e.opposite()] = dirs[(dir + 2)%4]
+            dir = (dir + a // 90) % 4
+        return ans
+    
+    def __repr__(self):
+        ext = '*' if self.exterior else ''
+        return list.__repr__(self) + ext
+
+
+class Faces(list):
+    def __init__(self, link):
+        self.link = link = link.copy()
+        list.__init__(self, [Face(link, F) for F in link.faces()])
+        F = max(self, key=len)
+        F.exterior = True
+        self.face_network = self.flow_networkx()
+        self.bend()
+        self.orient_edges()
+        self.edges = sum([F for F in self], [])
+        strands = {e.crossing for e in self.edges
+                      if isinstance(e.crossing, Strand)}
+        self.strand_CEPs =  [CrossingEntryPoint(s, 0) for s in strands]
+        
+    
+    def flow_networkx(faces):
+        """
+        Tamassia's associated graph N(P) where the flow problem resides.
+        """
+        G = networkx.DiGraph()
+        
+        # Source
+        source_demand = sum(F.source_capacity() for F in faces)
+        G.add_node('s', demand = -source_demand)
+        for i, F in enumerate(faces):
+            if F.source_capacity():
+                G.add_edge('s', i, weight=0, capacity=F.source_capacity())
+
+        # Sink
+        sink_demand = sum(F.sink_capacity() for F in faces)
+        assert sink_demand == source_demand
+        G.add_node('t', demand = sink_demand)
+        for i, F in enumerate(faces):
+            if F.sink_capacity():
+                G.add_edge(i, 't', weight=0, capacity=F.sink_capacity())
+
+        # Rest of edges
+        for A in faces:
+            for B in faces:
+                if A != B and A.edge_of_intersection(B):
+                    G.add_edge(faces.index(A), faces.index(B), weight=1)  # infinite capacity
+
+        return G
+
+    def bend(self):
+        N = self.face_network
+        flow = networkx.min_cost_flow(N)
+        for a, flows in flow.iteritems():
+            for b, w_a in flows.iteritems():
+                if w_a and {'s', 't'}.isdisjoint({a, b}):
+                    w_b = flow[b][a]
+                    A, B = self[a], self[b]
+                    e_a, e_b = A.edge_of_intersection(B)
+                    angles_a = w_a*[90] + w_b*[270]
+                    angles_b = w_b*[90] + w_a*[270]
+                    self.link._subdivide_edge(e_a, len(angles_a))
+                    A.bend(e_a, angles_a)
+                    B.bend(e_b, angles_b)
+
+    def orient_edges(self):
+        """
+        For each edge in a face, assign it one of four possible orientations:
+        "left", "right", "up", "down".
+        """
+        orientations = {self[0][0]:'right'}
+        N = self.face_network.to_undirected()
+        N.remove_node('s'), N.remove_node('t')
+        for i in networkx.traversal.dfs_preorder_nodes(N, 0):
+            F = self[i]
+            for edge in F:
+                if orientations.has_key(edge):
+                    new_orientations = F.orient_edges(edge, orientations[edge])
+                    for e, dir in new_orientations.iteritems():
+                        if orientations.has_key(e):
+                            assert orientations[e] == dir
+                        else:
+                            orientations[e] = dir
+                    break
+
+        assert len(orientations) == sum(len(F) for F in self)
+        self.orientations = orientations
+
+    def orthogonal_rep(self):
+        orientations = self.orientations
+        spec = [[ (e.crossing, e.opposite().crossing) for e in self.edges if orientations[e] == dir] for dir in ['right', 'up']]
+        ans = OrthogonalRep(*spec)
+
+        face_sizes = sorted(len(F) for F in self)
+        new_face_sizes = sorted(len(F) for F in ans.faces)
+        assert face_sizes == new_face_sizes
+        return ans
+
+
+                
+    def break_into_arrows(self):
+        arrows = []
+        for s in self.strand_CEPs:
+            arrow = [s, s.next()]
+            while not isinstance(arrow[-1].crossing, Strand):
+                arrow.append(arrow[-1].next())
+            arrows.append(arrow)
+
+        undercrossings = dict()
+        for i, arrow in enumerate(arrows):
+            for a in arrow[1:-1]:
+                if a.is_under_crossing():
+                    undercrossings[a] = i
+
+        crossings = []    # (under arrow, over arrow)
+        for i, arrow in enumerate(arrows):
+            for a in arrow[1:-1]:
+                if a.is_over_crossing():
+                    crossings.append( (undercrossings[a.other()], i) )
+            
+        return arrows, crossings
+
+    def plink_data(self, spacing = 25):
+        """
+        Returns:
+        * a list of vertex positions
+        * a list of arrows join vertices
+        * a list of crossings in the format (arrow over, arrow under)
+        """
+        emb = self.orthogonal_rep().basic_grid_embedding()
+        vertex_positions = []
+        for v in self.strand_CEPs:
+            a, b = emb[v.crossing]
+            vertex_positions.append( (spacing*(a+1), spacing*(b+1)) )
+
+        vert_indices = {v:i for i, v in enumerate(self.strand_CEPs)}
+        arrows, crossings = self.break_into_arrows()
+        arrows = [ (vert_indices[a[0]], vert_indices[a[-1]]) for a in arrows]
+        return vertex_positions, arrows, crossings
+        
+
+#----- Orhogonal representations ------
    
 class OrthogonalFace(list):
     """
-    A face of an OrthogonalRep oriented clockwise
-    and stored as a list of pairs (edge, vertex) where
-    vertex is gives the clockwise orientation of edge.
+    A face of an OrthogonalRep oriented *clockwise* and stored as a list of
+    pairs (edge, vertex) where vertex is gives the *clockwise* orientation
+    of edge.
     """
     def __init__(self, graph, (edge, vertex)):
         self.append( (edge, vertex) )
@@ -196,27 +345,29 @@ class OrthogonalFace(list):
         
 class OrthogonalRep(spherogram.Digraph):
     """
-    An orthogonal representation is an equivalence class
-    of planar embeddings of a graph where all edges are
-    either vertical or horizontal.   We assume there are
-    no degree 1 vertices.  
-
-    Horizontal edges are oriented to the right, and vertical
-    edges oriented upwards.
-
+    An orthogonal representation is an equivalence class of planar
+    embeddings of a graph where all edges are either vertical or
+    horizontal. We assume there are no degree 1 vertices.
+    
+    Horizontal edges are oriented to the right, and vertical edges
+    oriented upwards.
+    
     >>> square = OrthogonalRep([(0, 1), (3, 2)], [(0, 3), (1,2)])
     """
-    def __init__(self, horizonal_pairs, vertical_pairs):
+    def __init__(self, horizonal_pairs=[], vertical_pairs=[]):
         spherogram.Digraph.__init__(self)
         for (a,b) in horizonal_pairs:
-            e = self.add_edge(a, b)
-            e.kind = 'horizontal'
+            self.add_edge(a, b, 'horizontal')
         for (a,b) in vertical_pairs:
-            e = self.add_edge(a, b)
-            e.kind = 'vertical'
+            self.add_edge(a, b, 'vertical')
 
         self._build_faces()
         self._make_turn_regular()
+
+    def add_edge(self, a, b, kind):
+        e = spherogram.Digraph.add_edge(self, a, b)
+        e.kind = kind
+        return e
 
     def link(self, vertex):
         """
@@ -251,10 +402,9 @@ class OrthogonalRep(spherogram.Digraph):
             v0, v1 = F[i][1], F[j][1]
             kind = random.choice( ('vertical', 'horizontal'))
             if len([e for e in self.incident_to(v0) if e.kind == kind]):
-                e = self.add_edge(v0, v1)
+                e = self.add_edge(v0, v1, kind)
             else:
-                e = self.add_edge(v1, v0)
-            e.kind = kind
+                e = self.add_edge(v1, v0, kind)
             dummy.add(e)
             for v in [v0, v1]:
                 F = OrthogonalFace(self, (e, v))
@@ -283,7 +433,10 @@ class OrthogonalRep(spherogram.Digraph):
         chain_coors = topological_numbering(D)
         return { v:chain_coors[D.vertex_to_chain[v]] for v in self.vertices }
 
-    def basic_grid_embedding(self):
+    def basic_grid_embedding(self, rotate=False):
+        """
+        Returns the positions of vertices under the grid embedding. 
+        """
         V = self.chain_coordinates('horizontal')
         H = self.chain_coordinates('vertical')
         return {v:(H[v], V[v]) for v in self.vertices}
@@ -297,6 +450,27 @@ class OrthogonalRep(spherogram.Digraph):
                   self.edges if not e in self.dummy]
         return sum(verts + edges, Graphics())
         
+#----- plink code --------
+
+from plink import Vertex, Arrow, Crossing
+
+def load_from_spherogram(self, link, spacing=25):
+    vertices, arrows, crossings = Faces(link).plink_data(spacing)
+    self.clear()
+    self.clear_text()
+    for (x, y) in vertices:
+        self.Vertices.append(Vertex(x, y, self.canvas))
+    for u, v in arrows:
+        U, V = self.Vertices[u], self.Vertices[v]
+        self.Arrows.append(Arrow(U, V, self.canvas))
+    for u, o in crossings:
+        U, O = self.Arrows[u], self.Arrows[o]
+        self.Crossings.append(Crossing(O, U))
+    
+    self.create_colors()
+    self.goto_start_state()
+
+plink.LinkEditor.load_from_spherogram = load_from_spherogram
 
 #----- testing code --------
 
@@ -312,6 +486,10 @@ def check_faces(link):
 def test_face_method(N):
     for i in xrange(N):
         check_faces(random_link())
+
+def element(S):
+    return list(S)[0]
+    
     
 unknot = spherogram.RationalTangle(1).numerator_closure()
 hopf = spherogram.RationalTangle(2).numerator_closure()
@@ -327,3 +505,9 @@ kinked = OrthogonalRep([ (0, 1), (1, 2), (3, 4), (6, 7) ],
 kinked2 = OrthogonalRep([ (0,1), (2,3), (4,5), (6,7)], [(1,2), (0, 4), (3,7), (5,6)])
 kinked3 = OrthogonalRep([ (0,1), (2,3), (4,5), (6,7)], [(1,2), (0, 8),
                                                         (8,4), (3,7), (5,6)])
+
+cs = CrossingStrand(element(trefoil.vertices), 0)
+
+BOR = OrthogonalRep([(0, 1), (2, 3), (3, 4), (5,6), (6,7), (8, 9)],
+                    [(0,3), (2,5), (3, 6), (4, 7), (6, 8), (1, 9)])
+faces = Faces(trefoil)
