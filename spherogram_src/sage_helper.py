0ea957abd05fd26d4849a91b4809439a422f6cb6
Nathan Dunfield <nathan@dunfield.info>
2015-02-02 17:27:53 -0600

    Improved Sage specific doctests/docstrings

--- spherogram_src/sage_helper.py
+++ spherogram_src/sage_helper.py
@@ -1,6 +1,11 @@
 """
 Helper code for dealing with additional functionality when Sage is
 present.
+
+Any method which works only in Sage should be decorated with
+"@sage_method" and any doctests (in Sage methods or not) which should
+be run only in Sage should be styled with input prompt "sage:" rather
+than the usual ">>>".
 """
 try:
     import sage.all
@@ -8,7 +13,7 @@ try:
 except:
     _within_sage = False
 
-import functools
+import functools, doctest, re
 
 class SageNotAvailable(Exception):
     pass
@@ -28,6 +33,10 @@ else:
         wrapper._sage_method = True
         return wrapper
 
+
+# Not currently used, but could be exploited by an interpeter to hide
+# sage_methods when in plain Python.
+
 def sage_methods(obj):
     ans = []
     for attr in dir(obj):
@@ -39,5 +48,34 @@ def sage_methods(obj):
             pass
     return ans
 
+# Used for doctesting
 
+if _within_sage:
+    class DocTestParser(doctest.DocTestParser):
+        def parse(self, string, name='<string>'):
+            string = re.subn('([\n\A]\s*)sage:', '\g<1>>>>', string)[0]
+            return doctest.DocTestParser.parse(self, string, name)
+else:
+    DocTestParser = doctest.DocTestParser
+
+
+def print_results(module, runner):
+    print(module.__name__ + ':')
+    print('   %s failures out of %s tests.' %  (runner.failures, runner.tries))
+    
+def doctest_modules(modules, verbose=False):
+    finder = doctest.DocTestFinder(parser=DocTestParser())
+    failures, tries = 0, 0
+    for module in modules:
+        runner = doctest.DocTestRunner(verbose=verbose)
+        for test in finder.find(module):
+            runner.run(test)
+        failures += runner.failures
+        tries += runner.tries
+        print_results(module, runner)
+    print('\nAll doctests:\n   %s failures out of %s tests.' % (failures, tries))
+    
+
+        
+    
 
