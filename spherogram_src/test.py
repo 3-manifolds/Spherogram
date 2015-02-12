8ab69793411b09a8387cc144e50630945525dc67
Nathan Dunfield <nathan@dunfield.info>
2015-02-11 20:35:12 -0600

    Fixed bug introduced in Python 3 compatibility tweak Improved doctesting

--- spherogram_src/test.py
+++ spherogram_src/test.py
@@ -1,10 +1,10 @@
 import spherogram, spherogram.links, spherogram.links.test
 import spherogram.links.simplify, spherogram.links.morse
 
-from spherogram.sage_helper import _within_sage, doctest_modules
+import spherogram.sage_helper as sage_helper
 import snappy, re, getopt, sys
 
-snappy.number.Number._accuracy_for_testing = 8
+
 modules = [spherogram.codecs.DT, spherogram.graphs, spherogram.presentations,
            spherogram.links.links, spherogram.links.links_base,
            spherogram.links.random_links, spherogram.links.orthogonal,
@@ -12,13 +12,15 @@ modules = [spherogram.codecs.DT, spherogram.graphs, spherogram.presentations,
 
 spherogram.links.links_base.Link.exterior = snappy._link_exterior
 
-if _within_sage:
-    snappy.Manifold.use_field_conversion('snappy')
-    snappy.ManifoldHP.use_field_conversion('snappy')
+def doctest_modules(verbose=False):
+    snappy.number.Number._accuracy_for_testing = 8
+    if sage_helper._within_sage:
+        snappy.Manifold.use_field_conversion('snappy')
+        snappy.ManifoldHP.use_field_conversion('snappy')
+    return sage_helper.doctest_modules(modules, verbose)
 
-
-optlist, args = getopt.getopt(sys.argv[1:], 'v', ['verbose'])
-verbose = len(optlist) > 0
-doctest_modules(modules, verbose)
-
-spherogram.links.test.run()
+if __name__ == '__main__':
+    optlist, args = getopt.getopt(sys.argv[1:], 'v', ['verbose'])
+    verbose = len(optlist) > 0
+    doctest_modules(verbose)
+    spherogram.links.test.run()
