52c8d80058d5ad951658eebffdaa5c96a9ad48a9
Nathan Dunfield <nathan@dunfield.info>
2017-11-15 20:18:32 -0600

    Allowed Link._build_components to tweak the optional component_starts when they fail the DT code condition.
    Also, fixed bug in Link.DT_code where it wouldn't return the flips in
    non-alpha DT mode.

--- spherogram_src/links/links_base.py
+++ spherogram_src/links/links_base.py
@@ -599,15 +599,47 @@ class Link(object):
         for C in self.crossings:
             ans += C.crossing_strands()
         return ans
-    
+
+    def _DT_convention_holds(self):
+        for C in self.crossings:
+            first, second, flip = C.DT_info()
+            if (first + second) % 2 != 1:
+                return False
+        return True
+            
     def _build_components(self, component_starts=None):
         """
         Each component is stored as a list of *entry points* to
         crossings.  The labeling of the entry points (equivalently
         oriented edges) is compatible with the DT convention that each
         crossing has both an odd and and even incoming strand.
+
+        If provided the component_starts must consist of one
+        CrossingEntryPoint per component. These need not satisfy the
+        DT convention, but if they don't then they will be shifted by
+        at most one along each component to ensure it.
+
+        >>> L = Link('L10a90').mirror()
+        >>> L.DT_code()
+        [(20, 12, 18, 16), (8, 2, 4, 6, 14, 10)]
+        >>> C0, C1 = L.link_components
+        >>> L._build_components([C0[2], C1[0]])
+        >>> L.DT_code()
+        [(12, 18, 16, 20), (6, 8, 2, 4, 14, 10)]
+        >>> L._build_components([C0[0], C1[2]])
+        >>> L.DT_code()
+        [(18, 10, 16, 14), (2, 4, 6, 12, 20, 8)]
+
+        Here is one where we have to shift:
+
+        >>> L._build_components([C0[0], C1[1]])
+        >>> L.DT_code()
+        [(18, 10, 16, 14), (2, 4, 6, 12, 20, 8)]
+        >>> L._build_components([C0[-1], C1[0]])
+        >>> L.DT_code()
+        [(20, 12, 18, 16), (8, 2, 4, 6, 14, 10)]
         """
-        if component_starts:
+        if component_starts is not None:
             component_starts = [cs.crossing.entry_points()[cs.strand_index % 2] 
                                 for cs in component_starts]
         remaining, components = OrderedSet( self.crossing_entries() ), LinkComponents()
@@ -649,6 +681,20 @@ class Link(object):
                     others.append(o)
             other_crossing_entries.append(others)
             remaining = remaining - OrderedSet(component)
+
+        if (component_starts is not None) and (not self._DT_convention_holds()):
+            # Specified starts turn out not to satisfy the DT
+            # condition, so we tweak them to ensure this.
+            self._build_components(component_starts=None)
+            new_starts = []
+            for cep in component_starts:
+                if cep.strand_label() % 2 == 0:
+                    new_starts.append(cep)
+                else:
+                    new_starts.append(cep.next())
+            self._build_components(component_starts=new_starts)
+            assert self._DT_convention_holds()
+            
         self.link_components = components
 
     def digraph(self):
@@ -992,9 +1038,12 @@ class Link(object):
             DT = ''.join([DT_alphabet[x] for x in init_data] + [DT_alphabet[x>>1] for x in sum(DT, tuple())])
             if flips:
                 DT += '.' + ''.join([repr(flip) for flip in the_flips])
-            DT = "DT[" + DT + "]"
-
-        return DT
+            return "DT[" + DT + "]"
+        else:
+            if flips:
+                return DT, the_flips
+            else:
+                return DT
 
     def peer_code(self):
         peer = dict( [ c.peer_info() for c in self.crossings] )
