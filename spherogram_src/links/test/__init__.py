b52d356679a1ecfaec7162be412ae29cbb817115
Nathan Dunfield <nathan@dunfield.info>
2019-05-03 21:41:32 +0000

    Added Jones poly regression tests

--- spherogram_src/links/test/__init__.py
+++ spherogram_src/links/test/__init__.py
@@ -61,9 +61,10 @@ class TestLinkFunctions(unittest.TestCase):
         self.L6a2 = spherogram.Link('L6a2')
         self.L6a4 = spherogram.Link('L6a4')
         self.L7a3 = spherogram.Link('L7a3')
+        self.L10n1 = spherogram.Link('L10n1')
 
         self.knots = [self.K3_1, self.K7_2, self.K8_3, self.K8_13]
-        self.links = [self.L2a1, self.L6a4, self.L6a2, self.L7a3]
+        self.links = [self.L2a1, self.L6a4, self.L6a2, self.L7a3, self.L10n1]
         self.all_links = self.knots + self.links
                
     def random_knot(self):
@@ -257,6 +258,21 @@ class TestLinkFunctions(unittest.TestCase):
             k2 = self.random_link()
             self.assertTrue(k2.white_graph().is_planar())
             repeat-=1
+
+    def testJonesPolynomial(self):
+        L = LaurentPolynomialRing(QQ,'q')
+        q = L.gen()
+        data = [('K3_1', ('q^3 + q - 1', -4)),
+                ('K7_2', ('q^7 - q^6 + 2*q^5 - 2*q^4 + 2*q^3 - q^2 + q - 1', -8)),
+                ('K8_3', ('q^8 - q^7 + 2*q^6 - 3*q^5 + 3*q^4 - 3*q^3 + 2*q^2 - q + 1', -4)),
+                ('K8_13', ('-q^8 + 2*q^7 - 3*q^6 + 5*q^5 - 5*q^4 + 5*q^3 - 4*q^2 + 3*q - 1', -3)),
+                ('L6a2', ('-q^6 + q^5 - 2*q^4 + 2*q^3 - 2*q^2 + q - 1', 1)),
+                ('L6a4', ('-q^6 + 3*q^5 - 2*q^4 + 4*q^3 - 2*q^2 + 3*q - 1', -3)),
+                ('L7a3', ('-q^7 + q^6 - 3*q^5 + 2*q^4 - 3*q^3 + 3*q^2 - 2*q + 1', -7)),
+                ('L10n1', ('q^8 - 2*q^7 + 2*q^6 - 4*q^5 + 3*q^4 - 3*q^3 + 2*q^2 - 2*q + 1', -2))]
+        for link_name, (poly, exp) in data:
+            link = getattr(self, link_name)
+            self.assertEqual(link.jones_polynomial(), L(poly)*q**exp)
 
 def run():
     test_montesinos.test(15)
