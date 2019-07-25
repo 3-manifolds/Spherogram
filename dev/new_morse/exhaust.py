598c837036ab61938357d31ab24133d52f988225
Nathan Dunfield <nathan@dunfield.info>
2019-07-24 20:29:37 -0500

    dev: new code for morse diagrams and jones poly

---
+++ dev/new_morse/exhaust.py
@@ -0,0 +1,407 @@
+import snappy, spherogram
+from spherogram import Strand, Crossing, Link
+import random
+
+def insert_space(point_dict, i):
+    """
+    Insert two new points which will be labeled i and i + 1.
+        """
+    ans = dict()
+    for a, v in point_dict.items():
+        if a >= i:
+            a = a + 2
+        ans[a] = v
+    return ans
+
+def remove_space(point_dict, i):
+    """
+    Remove the points i and i + 1
+    """
+    ans = dict()
+    for a, v in point_dict.items():
+        if a < i:
+            ans[a] = v
+        if a > i + 1:
+            ans[a - 2] = v
+    return ans
+
+class Event(object):
+    """
+    There are three kinds of events:
+
+    * ('cup', a, b, c=None): Introduce a new oriented strand with
+      endpoints a and b oriented from a to b.  The optional argument c
+      is the index of the link component in the final link.
+
+    * ('cap', a, b): Join these strands by a cap.  The order of a and
+      b is not important.
+
+    * ('cross', a, b): Cross strand a *over* strand b.
+    """
+    def __init__(self, kind, a, b, c=None):
+        if not kind in ['cup', 'cap', 'cross']:
+            raise ValueError('Event kind must be cup, cap, or cross')
+        self.kind = kind
+        if abs(a - b) != 1 or a < 0 or b < 0:
+            raise ValueError('Invalid strand info')
+        self.a, self.b, self.c = a, b, c
+        self.min = min(a, b)
+        self.max = max(a, b)
+
+    def __repr__(self):
+        ans = (self.kind, self.a, self.b)
+        if self.c is not None:
+            ans += (self.c,)
+        return repr(ans)
+
+class MorseEncoding(object):
+    """
+    A MorseEncoding is a concrete encoding of a Morse diagram of an
+    oriented link in the sprit of:
+
+    http://katlas.org/wiki/MorseLink_Presentations
+
+    as a sequence of events.  At each stage, there are n active strand
+    ends labeled in order [0, 1, ... , n - 1].  The events can be
+    specified either as instances of Event or as tuples as shown
+    below.
+
+    >>> events = 2*[('cup', 0, 1)] + 2*[('cross', 1, 2)] + 2*[('cross', 3, 2)]
+    >>> events += [('cap', 1, 2), ('cap', 0, 1)]
+    >>> me = MorseEncoding(events)
+    >>> me.events[1:3]
+    [('cup', 0, 1), ('cross', 1, 2)]
+    >>> me.width
+    4
+    >>> me_copy = MorseEncoding(me.events)
+    >>> L = me.link()
+    >>> L.exterior().identify()[0]
+    m004(0,0)
+    >>> U = MorseEncoding([('cup', 0, 1), ('cap', 0, 1)]).link()
+    >>> U.exterior().fundamental_group().num_generators()
+    1
+    """
+    def __init__(self, events):
+        self.events = []
+        for event in events:
+            if not isinstance(event, Event):
+                event = Event(*event)
+            self.events.append(event)
+        self._check_and_set_width()
+
+    def _check_and_set_width(self):
+        width = 0
+        max_width = 0
+        for event in self.events:
+            if event.kind == 'cup':
+                assert event.max < width + 2
+                width += 2
+                max_width = max(width, max_width)
+            elif event.kind == 'cap':
+                assert event.max < width
+                width += -2
+            elif event.kind == 'cross':
+                assert event.max < width
+        assert width == 0
+        self.width = max_width
+
+    def link(self):
+        active = dict()
+        component_starts = []
+        crossings = []
+        for event in self.events:
+            if event.kind == 'cup':
+                S = Strand()
+                crossings.append(S)
+                active = insert_space(active, event.min)
+                active[event.a] = S[0]
+                active[event.b] = S[1]
+            elif event.kind == 'cap':
+                S = Strand()
+                crossings.append(S)
+                S[0] = active[event.a]
+                S[1] = active[event.b]
+                active = remove_space(active, event.min)
+            elif event.kind == 'cross':
+                C = Crossing()
+                crossings.append(C)
+                if event.a < event.b:
+                    C[3] = active[event.a]
+                    C[0] = active[event.b]
+                    active[event.a] = C[2]
+                    active[event.b] = C[1]
+                else:
+                    C[3] = active[event.a]
+                    C[2] = active[event.b]
+                    active[event.a] = C[0]
+                    active[event.b] = C[1]
+        return Link(crossings)
+
+    def __iter__(self):
+        return self.events.__iter__()
+
+class BiDict(object):
+    """
+    A bijective mapping from range(n) to a set of hashable non-integers.
+
+    >>> bd = BiDict({0:'a', 1:'b', 2:'c', 3:'d'})
+    >>> bd[0], bd['b']
+    ('a', 1)
+    >>> bd._check()
+    True
+    >>> bd.insert_space(4, 2)
+    >>> bd._check()
+    False
+    >>> bd[4] = 'e'
+    >>> bd['f'] = 5
+    >>> bd._check()
+    True
+    >>> bd
+    BiDict({0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f'})
+    >>> bd.insert_space(3, 2)
+    >>> bd[3], bd['y'] = 'x', 4
+    >>> bd
+    BiDict({0: 'a', 1: 'b', 2: 'c', 3: 'x', 4: 'y', 5: 'd', 6: 'e', 7: 'f'})
+    >>> bd.pop(1); bd.pop('c'); bd.pop('e'); bd
+    BiDict({0: 'a', 1: 'x', 2: 'y', 3: 'd', 4: 'f'})
+    >>> (0 in bd, 'x' in bd, 'z' in bd, 10 in bd)
+    (True, True, False, False)
+    >>> bd.insert_space(-1, 2); bd[0] = 'u'; bd[1] = 'v'; bd
+    BiDict({0: 'u', 1: 'v', 2: 'a', 3: 'x', 4: 'y', 5: 'd', 6: 'f'})
+    >>> sorted(bd.values())
+    ['a', 'd', 'f', 'u', 'v', 'x', 'y']
+    """
+    def __init__(self, int_to_set_dict):
+        self.n = n = len(int_to_set_dict)
+        assert sorted(int_to_set_dict.keys()) == list(range(n))
+        self.int_to_set = int_to_set_dict
+        self.set_to_int = {v:k for k, v in int_to_set_dict.items()}
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self.int_to_set[index]
+        elif index in self.set_to_int:
+            return self.set_to_int[index]
+
+    def __setitem__(self, index, value):
+        if isinstance(index, int):
+            assert not isinstance(value, int)
+            the_int, the_set = index, value
+        else:
+            assert isinstance(value, int)
+            the_int, the_set = value, index
+        assert 0 <= the_int < self.n
+        self.int_to_set[the_int] = the_set
+        self.set_to_int[the_set] = the_int
+
+    def __contains__(self, value):
+        return (value in self.set_to_int) or (value in self.int_to_set)
+
+    def pop(self, index):
+        """
+        Remove the given item and shift the indices downwards when necessary
+        to close the gap.
+        """
+        if isinstance(index, int):
+            the_int = index
+            the_set = self.int_to_set[the_int]
+        else:
+            the_set = index
+            the_int = self.set_to_int[the_set]
+        self.int_to_set.pop(the_int)
+        self.set_to_int.pop(the_set)
+        def shift(j):
+            return j if j < the_int else j - 1
+        self.int_to_set = {shift(k):v for k, v in self.int_to_set.items()}
+        self.set_to_int = {k:shift(v) for k, v in self.set_to_int.items()}
+        self.n += -1
+
+    def insert_space(self, i, n):
+        """
+        Shift indices upwards when necessary so that the n slots
+
+        i, i+1, ... , i + n - 1
+
+        are unassigned.
+        """
+        assert isinstance(i, int)
+        def shift(j):
+            return j if j < i else j + n
+        self.n = self.n + n
+        self.int_to_set = {shift(k):v for k, v in self.int_to_set.items()}
+        self.set_to_int = {k:shift(v) for k, v in self.set_to_int.items()}
+
+    def values(self):
+        return self.int_to_set.values()
+
+    def __repr__(self):
+        itos = self.int_to_set
+        keys = sorted(itos.keys())
+        items = ['%d: %s' % (k, repr(itos[k])) for k in keys]
+        return 'BiDict({' + ', '.join(items) + '})'
+
+    def __len__(self):
+        return len(self.int_to_set)
+
+    def _check(self):
+        return sorted(self.int_to_set.keys()) == list(range(self.n))
+
+def is_range(L):
+    """
+    >>> is_range([2, 3, 4]), is_range([2, 3, 5])
+    (True, False)
+    """
+    return L == list(range(min(L), max(L) + 1))
+
+class Frontier(BiDict):
+    def overlap_indices(self, crossing):
+        neighbors = [cs.opposite() for cs in crossing.crossing_strands()]
+        return [self[ns] for ns in neighbors if ns in self]
+
+    def overlap_is_consecutive(self, crossing):
+        overlap = self.overlap_indices(crossing)
+        return len(overlap) > 0 and is_range(overlap)
+
+    def biggest_all_consecutive_overlap(self, crossings):
+        possible = []
+        for crossing in crossings:
+            overlap = list(sorted(self.overlap_indices(crossing)))
+            if len(overlap) > 0 and is_range(overlap):
+                possible.append((len(overlap), min(overlap), crossing))
+        overlap_len, start, crossing = max(possible)
+        return overlap_len, start, crossing
+
+
+def num_overlap(crossing, frontier):
+    return len([ns for ns in neighbors if ns in frontier])
+
+class MorseExhaustion(object):
+    """
+    An exhaustion of a link where crossings are added in one-by-one
+    so that the resulting tangle is connected at every stage.
+
+    Starting at the given crossing, it uses a greedy algorithm to try
+    to minimize the sizes of the frontiers of the intermediate tangles.
+
+    The answer is returned as a sequence of events describing a
+    MorseEncoding.
+
+    If no initial crossing is specified, one is chosen at random.
+
+    >>> L = Link('L2a1')
+    >>> mexhaust = MorseExhaustion(L, L.crossings[0])
+    >>> mexhaust
+    [('cup', 0, 1), ('cup', 0, 1), ('cross', 1, 2), ('cross', 1, 2), ('cap', 0, 1), ('cap', 0, 1)]
+    >>> me = MorseEncoding(mexhaust)
+    >>> me.link().exterior().fundamental_group().relators()
+    ['abAB']
+    """
+    def __init__(self, link, crossing=None):
+        events = []
+        if crossing is None:
+            crossing = random.choice(link.crossings)
+        crossings = [crossing]
+        events = [('cup', 0, 1), ('cup', 0, 1), ('cross', 1, 2)]
+        css = crossing.crossing_strands()
+        frontier = Frontier({0:css[3], 1:css[2], 2:css[1], 3:css[0]})
+        frontier_lengths = [4]
+        while len(crossings) < len(link.crossings):
+            next_crossings = {cs.opposite()[0] for cs in frontier.values()}
+            overlap, i, C = frontier.biggest_all_consecutive_overlap(next_crossings)
+            cs = frontier[i]
+            cs_opp = cs.opposite()
+            assert C not in crossings
+            crossings.append(C)
+            if overlap == 1:
+                i = frontier[cs]
+                events.append(('cup', i+1, i+2))
+                if cs_opp.strand_index in {1, 3}:
+                    events.append(('cross', i, i+1))
+                else:
+                    events.append(('cross', i+1, i))
+                frontier.insert_space(i+1, 2)
+                for s in range(3):
+                    frontier[i + s] = cs_opp.rotate(-(s + 1))
+            elif overlap == 2:
+                if cs_opp.strand_index in {1, 3}:
+                    events.append(('cross', i, i+1))
+                else:
+                    events.append(('cross', i+1, i))
+                for s in range(2):
+                    frontier[i + s] = cs_opp.rotate(-(s + 1))
+            elif overlap == 3:
+                if cs_opp.strand_index in {1, 3}:
+                    events.append(('cross', i, i+1))
+                else:
+                    events.append(('cross', i+1, i))
+                events.append(('cap', i+1, i+2))
+                frontier.pop(i + 2)
+                frontier.pop(i)
+                frontier[i] = cs_opp.rotate(-1)
+            else:
+                assert overlap == 4
+                if cs_opp.rotate().strand_index in {1, 3}:
+                    events.append(('cross', i+1, i+2))
+                else:
+                    events.append(('cross', i+2, i+1))
+                events += 2*[('cap', i, i+1)]
+                for a in range(4):
+                    frontier.pop(i)
+            assert frontier_lengths[-1] + 4 - 2*overlap == len(frontier)
+            assert frontier._check()
+            frontier_lengths.append(len(frontier))
+
+        self.link = link
+        self.crossings = crossings
+        self.frontier_lengths = frontier_lengths
+        self.events = events
+        self.width = max(frontier_lengths)//2
+
+    def __repr__(self):
+        return repr(self.events)
+
+    def __iter__(self):
+        return self.events.__iter__()
+
+def good_exhaustion(link, max_failed_tries=20):
+    """
+    Uses a random search to try to find an Exhaustion with small width.
+
+    >>> ge = good_exhaustion(Link('K4a1'))
+    >>> ge.width
+    2
+    """
+    crossings = list(link.crossings)
+    C = random.choice(crossings)
+    crossings.remove(C)
+    E_best = MorseExhaustion(link, C)
+    tries = 0
+    while len(crossings) and tries < max_failed_tries:
+        C = random.choice(crossings)
+        crossings.remove(C)
+        E = MorseExhaustion(link, C)
+        if E.width < E_best.width:
+            E_best = E
+        tries += 1
+    return E_best
+
+def test_morse_machine(link):
+    E = link.exterior()
+    exhaust = MorseExhaustion(link, link.crossings[0])
+    encoding = MorseEncoding(exhaust)
+    new_link = encoding.link()
+    new_E = new_link.exterior()
+    return E.is_isometric_to(new_E)
+
+def test_many(N):
+    """
+    >>> test_many(5)
+    """
+    for i in range(N):
+        M = snappy.HTLinkExteriors().random()
+        if M.solution_type() == 'all tetrahedra positively oriented':
+            assert test_morse_machine(M.link())
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
---
+++ dev/new_morse/new_jones.py
@@ -0,0 +1,202 @@
+"""
+Conventions follow Jake's lectures at PCMI, Summer 2019.
+"""
+
+from sage.all import ZZ, LaurentPolynomialRing, PerfectMatchings, PerfectMatching
+import exhaust
+from spherogram import Link
+import snappy
+
+R = LaurentPolynomialRing(ZZ, 'q')
+q = R.gen()
+
+def num_Pn(n):
+    """    
+    An element of Jake's P_{0, n} of planar tangles from 0 points to n
+    points will be a PerfectMatching of [0,..,n-1] where
+    is_noncrossing is True.
+    """
+    return len([m for m in PerfectMatchings(n) if m.is_noncrossing()])
+
+def insert_cup(matching, i):
+    """
+    Insert a new adjacent matching which joins i and i + 1.
+
+    >>> m = PerfectMatching([(0, 1), (2, 5), (3, 4)])
+    >>> insert_cup(m, 0)
+    [(0, 1), (2, 3), (4, 7), (5, 6)]
+    >>> insert_cup(m, 1)
+    [(0, 3), (1, 2), (4, 7), (5, 6)]
+    >>> insert_cup(m, 6)
+    [(0, 1), (2, 5), (3, 4), (6, 7)]
+    """
+    assert len(matching.base_set()) >= i
+    def shift(pair):        
+        return [a if a < i else a + 2 for a in pair]
+    return PerfectMatching([shift(pair) for pair in matching] + [(i, i + 1)])
+
+def cap_off(matching, i):
+    """
+    Merge i and i + 1 with a cap.  Returns a new matching and whether
+    or not a circle was created.
+
+    >>> m = PerfectMatching([(0, 5), (1, 4), (2, 3)])
+    >>> cap_off(m, 2)
+    ([(0, 3), (1, 2)], True)
+    >>> cap_off(m, 3)
+    ([(0, 3), (1, 2)], False)
+    """
+    def shift(a):
+        return a if a < i else a - 2
+    def follow(a):
+        b = matching.partner(a)
+        if b == i:
+            b = matching.partner(i + 1)
+        elif b == i + 1:
+            b = matching.partner(i)
+        return b
+
+    n = len(matching.base_set())
+    circle = matching.partner(i) == i + 1
+    new_match = set()
+    for a in range(n):
+        if not a in {i, i + 1}:
+            u, v = shift(a), shift(follow(a))
+            if u > v:
+                u, v = v, u
+            new_match.add((u, v))
+    return PerfectMatching(new_match), circle
+            
+class VElement(object):
+    """
+    An element of some V_{0, n} which is the free R-module on P_{0, n}
+
+    >>> m = PerfectMatching([(0, 1), (3, 4), (2, 5)])
+    >>> v1 = VElement(m)
+    >>> v1
+    (1)*[(0, 1), (2, 5), (3, 4)]
+    >>> v2 = (q + q**-1)*v1
+    >>> v2
+    (q^-1 + q)*[(0, 1), (2, 5), (3, 4)]
+    >>> v3 = q* VElement(PerfectMatching([(5, 0), (4, 3), (1, 2)]))
+    >>> v1 + v2 + v3
+    (q^-1 + 1 + q)*[(0, 1), (2, 5), (3, 4)] + (q)*[(0, 5), (1, 2), (3, 4)]
+    >>> v2.insert_cup(6)
+    (q^-1 + q)*[(0, 1), (2, 5), (3, 4), (6, 7)]
+    >>> (v1 + v2 + v3).cap_off(1)
+    (q^-1 + 2 + q + q^2)*[(0, 3), (1, 2)]
+    """
+    def __init__(self, spec=None):
+        self.dict = dict()
+        if spec is None:
+            spec = PerfectMatching([])
+        if isinstance(spec, dict):
+            self.dict = spec
+        if isinstance(spec, PerfectMatching):
+            assert spec.is_noncrossing()
+            self.dict[spec] = R.one()
+
+    def __rmul__(self, other):
+        if other in R:
+            other = R(other)
+            return VElement({m:other*c for m, c in self.dict.items()})
+
+    def __add__(self, other):
+        if isinstance(other, VElement):
+            ans_dict = self.dict.copy()
+            for matching, coeff in other.dict.items():
+                cur_coeff = self.dict.get(matching, R.zero())
+                ans_dict[matching] = cur_coeff + coeff
+        return VElement(ans_dict)
+
+    def __repr__(self):
+        matchings = sorted(self.dict.keys())
+        terms = ['(%s)*%s' % (self.dict[m], m) for m in matchings]
+        if len(terms) == 0:
+            return '0'
+        return ' + '.join(terms)
+
+    def insert_cup(self, i):
+        """
+        Insert an new matching at (i, i + 1)
+        """
+        return VElement({insert_cup(m, i):c for m, c in self.dict.items()})
+
+    def cap_off(self, i):
+        ans_dict = dict()
+        for matching, coeff in self.dict.items():
+            new_matching, has_circle = cap_off(matching, i)
+            cur_coeff = ans_dict.get(new_matching, R.zero())
+            if has_circle:
+                coeff = (q + q**-1)*coeff
+            ans_dict[new_matching] = cur_coeff + coeff
+        return VElement(ans_dict)
+
+    def cap_then_cup(self, i):
+        return self.cap_off(i).insert_cup(i)
+
+    def add_positive_crossing(self, i):
+        return self + (-q)*self.cap_then_cup(i)
+
+    def add_negative_crossing(self, i):
+        return self.cap_then_cup(i) + (-q)*self
+
+    def is_multiple_of_empty_pairing(self):
+        return len(self.dict) == 1 and (PerfectMatching([]) in self.dict)
+
+
+def kauffman_bracket(link):
+    """
+    >>> L = Link('T(2, 3)')
+    >>> kauffman_bracket(L)
+    q^-2 + 1 + q^2 - q^6
+    """
+    ans = VElement()
+    if isinstance(link, exhaust.MorseEncoding):
+        encoded = link
+    else:
+        exhaustion = exhaust.MorseExhaustion(link)
+        encoded = exhaust.MorseEncoding(exhaustion)
+    for event in encoded:
+        if event.kind == 'cup':
+            ans = ans.insert_cup(event.min)
+        elif event.kind == 'cap':
+            ans = ans.cap_off(event.min)
+        else:
+            assert event.kind == 'cross'
+            if event.a < event.b:
+                ans = ans.add_positive_crossing(event.min)
+            else:
+                ans = ans.add_negative_crossing(event.min)
+    assert ans.is_multiple_of_empty_pairing()
+    return ans.dict[PerfectMatching([])]
+
+def jones_polynomial(link, normalized=True):
+    bracket = kauffman_bracket(link)
+    if normalized:
+        factor = q + q**-1
+        norm_bracket = bracket // factor
+        assert norm_bracket*factor == bracket
+    else:
+        norm_bracket = bracket
+
+    signs = [c.sign for c in link.crossings]
+    n_minus, n_plus = signs.count(-1), signs.count(1)  
+    return (-1)**n_minus * q**(n_plus - 2*n_minus) * norm_bracket
+
+def test_one_link(link):
+    new_poly = jones_polynomial(link)
+    old_poly = link.jones_polynomial(new_convention=True)
+    return new_poly - old_poly == 0
+
+def test_links(N):
+    for i in range(N):
+        M = snappy.HTLinkExteriors.random()
+        L = M.link()
+        print(M.name(), test_one_link(L))
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
+                
+        
