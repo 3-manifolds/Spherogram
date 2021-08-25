9b170c108f64ed7ecd870fd9947f5bc77e709b08
Nathan Dunfield <nathan@dunfield.info>
2021-08-25 09:05:35 -0500

    Updated knot_floer_homology functionality and requirements

--- spherogram_src/links/invariants.py
+++ spherogram_src/links/invariants.py
@@ -306,7 +306,7 @@ class Link(links_base.Link):
             else:
                 return p
 
-    def knot_floer_homology(self):
+    def knot_floer_homology(self, prime=2, complex=False):
         """
         Uses Zoltán Szabó's HFK Calculator to compute the knot Floer
         homology.  This also gives the Seifert genus, whether the knot
@@ -338,8 +338,39 @@ class Link(links_base.Link):
         >>> ranks[3, 3], ranks[3, 4]
         (1, 1)
 
-        Computation is done over F_2, though the underlying
-        knot_floer_homology module can handle other finite fields.
+        Computation is done over F_2 by default, other primes less
+        than 2^15 can be used instead via the optional "prime"
+        parameter.
+
+        If the parameter `complex` is set to True, then the simplified
+        "UV = 0" knot Floer chain complex is returned. This complex is
+        computed over the ring F[U,V]/(UV = 0), where F is the integers
+        mod the chosen prime; this corresponds to only the horizontal and
+        vertical arrows in the full knot Floer complex. The complex is
+        specified by:
+
+        * generators: a dictionary from the generator names to their
+          (Alexander, Maslov) gradings.  The number of generators is
+          equal to the total_rank.
+
+        * differential: a dictionary whose value on (a, b) is an integer
+          specifying the coefficient on the differential from generator a
+          to generator b, where only nonzero differentials are
+          recorded. (The coefficient on the differential is really an
+          element of F[U,V]/(UV = 0), but the power of U or V can be
+          recovered from the gradings on a and b so only the element of F
+          is recorded.)
+
+        For example, to compute the vertical differential, whose homology
+        is HFhat(S^3), you can do:
+
+        >>> data = pd_to_hfk(pd_code, complex=True)
+        >>> gens, diff = data['generators'], data['differentials']
+        >>> vert = {(i,j):diff[i, j] for i, j in diff
+        ...                          if gens[i][1] == gens[j][1] + 1}
+        >>> len(vert)
+        2
+
         """
         import knot_floer_homology
         if len(self.link_components) + self.unlinked_unknot_components > 1:
